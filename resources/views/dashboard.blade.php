740071b550a41529552b47b0153681177275d69d
Ильдар Суфияров <ildarmyname@gmail.com>
2026-04-24 12:32:00 +0500

    small fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -145,7 +145,7 @@
                                     <a href="/tasks/{{$task->id}}" class="p-1.5 text-gray-400 hover:text-indigo-600 hover:bg-indigo-50 rounded-lg transition-colors" title="Детали">
                                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
                                     </a>
-                                    @if($userRole === "teacher" && $task->teacher_id === auth()->user()->id)
+                                    @if($userRole === "teacher" && $task->teacher_id === auth()->user()->id && $task->status === "pending")
                                         <button onclick="openConfirmationModal({{ $task->id }}, '{{ addslashes($task->title) }}')"  class="p-1.5 text-gray-400 hover:text-red-600 hover:bg-red-50 rounded-lg transition-colors">
                                             <svg style="width: 16px; height: 16px" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash"><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
                                         </button>
